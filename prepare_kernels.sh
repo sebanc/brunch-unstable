f66afd0c34e8e87a1b17dea7a0c1af1d53c11233
P4rfa1t <166934579+P4rfa1t@users.noreply.github.com>
2025-04-06 04:17:31 +0800

    Update prepare_kernels.sh

--- prepare_kernels.sh
+++ prepare_kernels.sh
@@ -43,26 +43,14 @@ for kernel in $kernels; do
 	echo "kernel_version=$kernel_version"
 	[ ! "x$kernel_version" == "x" ] || { echo "Kernel version not found"; exit 1; }
 	case "$kernel" in
-		6.12|6.6)
+		6.12)
 			echo "Downloading ChromiumOS kernel source for kernel $kernel version $kernel_version from https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/$kernel_remote_path$kernel.tar.gz"
 			curl -L "https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/$kernel_remote_path$kernel.tar.gz" -o "./kernels/chromiumos-$kernel.tar.gz" || { echo "Kernel source download failed"; exit 1; }
-			mkdir "./kernels/chromebook-$kernel" "./kernels/$kernel"
-			tar -C "./kernels/chromebook-$kernel" -zxf "./kernels/chromiumos-$kernel.tar.gz" || { echo "Kernel $kernel source extraction failed"; exit 1; }
+			mkdir "./kernels/$kernel"
 			tar -C "./kernels/$kernel" -zxf "./kernels/chromiumos-$kernel.tar.gz" || { echo "Kernel $kernel source extraction failed"; exit 1; }
 			rm -f "./kernels/chromiumos-$kernel.tar.gz"
-			apply_patches "chromebook-$kernel"
-			make_config "chromebook-$kernel" "chromebook"
 			apply_patches "$kernel"
 			make_config "$kernel" "generic"
-		;;
-		*)
-			echo "Downloading ChromiumOS kernel source for kernel $kernel version $kernel_version from https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/$kernel_remote_path$kernel.tar.gz"
-			curl -L "https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/$kernel_remote_path$kernel.tar.gz" -o "./kernels/chromiumos-$kernel.tar.gz" || { echo "Kernel source download failed"; exit 1; }
-			mkdir "./kernels/chromebook-$kernel"
-			tar -C "./kernels/chromebook-$kernel" -zxf "./kernels/chromiumos-$kernel.tar.gz" || { echo "Kernel $kernel source extraction failed"; exit 1; }
-			rm -f "./kernels/chromiumos-$kernel.tar.gz"
-			apply_patches "chromebook-$kernel"
-			make_config "chromebook-$kernel" "chromebook"
 		;;
 	esac
 done
@@ -72,6 +60,6 @@ rm -rf ./kernels
 mkdir ./kernels
 
 chromeos_version="R135"
-kernels="6.6 6.12"
+kernels="6.12"
 download_and_patch_kernels
 
